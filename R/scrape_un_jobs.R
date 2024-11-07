7dd241f6010d059500dd30f7e7518a3e6be24d66
Henry Dehe <henrydehe@outlook.com>
2024-11-07 19:15:16 +0000

    Added functions to deal with potential errors when scraping. - reliably() handles time and tries again when download fails - warnings_to_gha() parses warnings to the gha output

--- R/scrape_un_jobs.R
+++ R/scrape_un_jobs.R
@@ -13,13 +13,35 @@ library(nanoparquet)
 library(gha)
 
 
+## Helper Functions --------------------------------------------------------
+
+# Combination of purrr funs so that the function runs slowly, on error tries
+# again, then returns a value
+reliably <- function(f,
+                     delay = rate_delay(),
+                     backoff = rate_backoff(),
+                     otherwise = NULL,
+                     quiet = FALSE) {
+  f |>
+    slowly(rate = delay) |>
+    insistently(rate = backoff, quiet = quiet) |>
+    possibly(otherwise = otherwise, quiet = FALSE)
+
+}
+
+warnings_to_gha <- function() {
+  if (!is.null(names(warnings()))) {
+    gha_warning(str_c(names(warnings()), collapse = "\n"))
+  }
+}
+
+
 # Functions for scraping latest jobs --------------------------------------
 
 get_un_jobs_list <- function(){
 
   # FUN: scrapes all jobs on a single page from unjobs.org
-  get_jobs_tbl <- function(url, pg_sleep = 0.01){
-    Sys.sleep(pg_sleep)
+  get_jobs_tbl <- function(url){
     html_jobs <- read_html(url) |>
       html_elements(".job") |>
       discard(~is.na(html_attr(.x, "id")))
@@ -43,6 +65,16 @@ get_un_jobs_list <- function(){
     tibble(id, job_title, org, posting_date, link)
   }
 
+  # Make the function reliable
+  delay <- rate_delay(pause = 0.01)
+  backoff <- rate_backoff(pause_base = 0.02,
+                          pause_cap = 10,
+                          pause_min = 0.01,
+                          max_times = 5)
+
+  get_jobs_tbl_reliably <- get_jobs_tbl |>
+    reliably(delay = delay, backoff = backoff)
+
   # https://unjobs.org/ has a limit of 40 pages you can consult
   maxpages <- 40
   # generate the urls for most recent posts
@@ -50,14 +82,16 @@ get_un_jobs_list <- function(){
   urls_unjobs_non_un <- str_glue("https://unjobs.org/non-un/{1:maxpages}")
 
   unjobs_un <- urls_unjobs_un |>
-    map(get_jobs_tbl) |>
+    map(get_jobs_tbl_reliably) |>
     bind_rows() |>
     mutate(is_un = TRUE)
 
   unjobs_non_un <- urls_unjobs_non_un |>
-    map(get_jobs_tbl) |>
+    map(get_jobs_tbl_reliably) |>
     bind_rows() |>
     mutate(is_un = FALSE)
+
+  warnings_to_gha()
 
   unjobs <- unjobs_un |>
     bind_rows(unjobs_non_un)
@@ -66,9 +100,7 @@ get_un_jobs_list <- function(){
 
 get_un_jobs_full <- function(un_jobs_list){
 
-  get_job_desc <- function(url, pg_sleep = 0.01) {
-
-    Sys.sleep(pg_sleep)
+  get_job_desc <- function(url) {
 
     job_desc <- read_html(url) |>
       html_element(".fp-snippet") |>
@@ -91,10 +123,26 @@ get_un_jobs_full <- function(un_jobs_list){
 
   }
 
-  un_jobs_list |>
-    mutate(descriptor = pmap(list(url = link), get_job_desc)) |>
+  delay <- rate_delay(pause = 0.01)
+  backoff <- rate_backoff(pause_base = 0.02,
+                          pause_cap = 10,
+                          pause_min = 0.01,
+                          max_times = 5)
+  otherwise <- tibble(country = character(),
+                      location = character(),
+                      job_desc = character())
+
+  get_job_desc_reliably <- get_job_desc |>
+    reliably(delay = delay, backoff = backoff, otherwise = otherwise)
+
+  un_jobs_full <- un_jobs_list |>
+    mutate(descriptor = pmap(list(url = link), get_job_desc_reliably)) |>
     unnest(descriptor) |>
     mutate(date_collected = Sys.Date())
+
+  warnings_to_gha()
+
+  return(un_jobs_full)
 
 }
 
@@ -129,17 +177,13 @@ scrape_un_jobs <- function(){
 
 gha_notice(str_glue("Initiating scrape_un_jobs on {Sys.Date()}"))
 
-if(!file.exists(str_glue("data/{Sys.Date()}.parquet"))) {
+if (!file.exists(str_glue("data/{Sys.Date()}.parquet"))) {
   scrape_un_jobs() |>
     {\(x) if (nrow(x) >= 1) write_parquet(x, str_glue("data/un_jobs/{Sys.Date()}.parquet"))}()
-  gha_notice("scrape_un_jobs Complete!")
+  gha_notice("Workflow Complete!")
 } else {
   gha_warning("scrape_un_jobs already run today, delete data file to run again")
 }
-
-
-
-
 
 
 
